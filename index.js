feb8cae9d177dc0d5cf1749c67797cb6ecd8d447
osha7 <ogroetz@gmail.com>
2021-04-26 14:54:29 -0600

    validations

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true, useUnifiedTopology: true }) //connects to MongoDB with connection string
-mongoose.connect('mongodb://localhost/mongo-musician', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }) //connects to MongoDB with connection string
-// this connection string will change when in production environment and will not be hardcoded, it will come from config file
-// .connect returns promise
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...', err))
+// // mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true, useUnifiedTopology: true }) //connects to MongoDB with connection string
+// mongoose.connect('mongodb://localhost/mongo-musician', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }) //connects to MongoDB with connection string
+// // this connection string will change when in production environment and will not be hardcoded, it will come from config file
+// // .connect returns promise
+//     .then(() => console.log('Connected to MongoDB...'))
+//     .catch(err => console.error('Could not connect to MongoDB...', err))
 
-const musicianSchema = new mongoose.Schema({
-    name: String,
-    genre: String,
-    tags: [ String ],
-    // date: Date
-    date: { type: Date, default: Date.now}, //we won't have to specify manually
-    isActive: Boolean,
-    bookingPrice: Number
-});
+// const musicianSchema = new mongoose.Schema({
+//     name: String,
+//     genre: String,
+//     tags: [ String ],
+//     // date: Date
+//     date: { type: Date, default: Date.now}, //we won't have to specify manually
+//     isActive: Boolean,
+//     bookingPrice: Number
+// });
 
-const Musician = mongoose.model('Musician', musicianSchema); // Musician is a Class not an object
+// const Musician = mongoose.model('Musician', musicianSchema); // Musician is a Class not an object
 
-// async function createCourse() {
+// async function createMusician() {
 //     // make new object:
 //     const musician = new Musician({
 //         name: "Phoebe Bridgers",
@@ -33,7 +33,7 @@ const Musician = mongoose.model('Musician', musicianSchema); // Musician is a Cl
 //     console.log("Result", result);
 // };
 
-// createCourse();
+// createMusician();
 
 // async function getMusicians() {
 //     const musicians = await Musician.find();
@@ -245,13 +245,376 @@ const Musician = mongoose.model('Musician', musicianSchema); // Musician is a Cl
 // updateMusician('6081f54f59c060e2afe7c7e5');
 
 
-// Deleting Documents:
-async function deleteMusician(id) { // Mosh uses function: removeMusician(id)
-    // const result = await Musician.deleteOne({ _id: id, }); // or: .deleteMany()
-    // console.log(result);
-    const musician = await Musician.findByIdAndRemove(id);
-    // returns 'null' if no musician by given id
-    console.log(musician);
-}
+// // Deleting Documents:
+// async function deleteMusician(id) { // Mosh uses function: removeMusician(id)
+//     // const result = await Musician.deleteOne({ _id: id, }); // or: .deleteMany()
+//     // console.log(result);
+//     const musician = await Musician.findByIdAndRemove(id);
+//     // returns 'null' if no musician by given id
+//     console.log(musician);
+// }
     
-deleteMusician('6081f54f59c060e2afe7c7e7');
+// deleteMusician('6081f54f59c060e2afe7c7e7');
+
+
+
+// ----------------------------VALIDATORS: --------------------------------------------------
+
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost/mongo-validates-musician', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }) 
+//     .then(() => console.log('Connected to MongoDB...'))
+//     .catch(err => console.error('Could not connect to MongoDB...', err))
+
+// const musicianSchema = new mongoose.Schema({
+//     name: { type: String, required: true }, // validates that name is always present & only meaningful in mongoose not mongodb
+//     genre: String,
+//     tags: [ String ],
+//     date: { type: Date, default: Date.now}, 
+//     isActive: Boolean,
+//     bookingPrice: Number
+// });
+
+// const Musician = mongoose.model('Musician', musicianSchema);
+
+// async function createMusician() {
+//     const musician = new Musician({
+//         //  name: "Phoebe Bridgers",
+//         genre: "Folk-rock",
+//         tags: ['Saddest Factory', 'Punisher'],
+//         isActive: true,
+//         bookingPrice: 10000
+//     });
+    
+//     try {
+//         // await musician.validate() // returns Promise<void> NOT a boolean 
+//         // await musician.validate((err) => {
+//         //     if (err) {}
+//         // })
+//         // const result = await musician.save();
+//         // console.log("Result", result);
+//     }
+//     catch (error) {
+//         console.log("Error Message", error.message)
+//     }
+// };
+
+// createMusician();
+
+// ------------------------BUILT IN VALIDATORS FOR STRINGS AND NUMBERS: ----------------------------------------------------
+
+
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost/mongo-validates-musician', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }) 
+//     .then(() => console.log('Connected to MongoDB...'))
+//     .catch(err => console.error('Could not connect to MongoDB...', err))
+
+// const musicianSchema = new mongoose.Schema({
+//     name: { 
+//         type: String, 
+//         required: true, // simple boolean
+//         minlength: 5,
+//         maxlength: 255, //characters
+//         // match: /pattern/ // regex
+//     },
+//     genre: {
+//         type: String,
+//         required: true,
+//         enum: ['rock', 'grunge', 'folk-rock', 'alternative-rock', 'punk'], // genre must always be one of these values, otherwise get a validation error
+//     }, 
+//     tags: [ String ],
+//     date: { type: Date, default: Date.now}, 
+//     isActive: Boolean,
+//     bookingPrice: {
+//         type: Number,
+//         // function to conditionally make a property required (in example below, only required if isActive === true )
+//         required: function() { return this.isActive; }, // can not replace this with Arrow function -> arrow function does not have a 'this'
+//         min: 1500,
+//         max: 50000
+//     }
+// });
+
+// const Musician = mongoose.model('Musician', musicianSchema);
+
+// async function createMusician() {
+//     const musician = new Musician({
+//         name: "Phoebe Bridgers",
+//         genre: "-",
+//         tags: ['Saddest Factory', 'Punisher'],
+//         isActive: false,
+//         bookingPrice: 10000
+//     });
+    
+//     try {
+//         const result = await musician.save();
+//         console.log("Result", result);
+//     }
+//     catch (error) {
+//         console.log("Error Message", error.message)
+//     }
+// };
+
+// createMusician();
+
+
+// ---------------------CUSTOM VALIDATOR:-----------------------------------------------
+
+
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost/mongo-validates-musician', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }) 
+//     .then(() => console.log('Connected to MongoDB...'))
+//     .catch(err => console.error('Could not connect to MongoDB...', err))
+
+// const musicianSchema = new mongoose.Schema({
+//     name: { 
+//         type: String, 
+//         required: true,
+//         minlength: 5,
+//         maxlength: 255,
+//     },
+//     genre: {
+//         type: String,
+//         required: true,
+//         enum: ['rock', 'grunge', 'folk-rock', 'alternative-rock', 'punk'], 
+//     }, 
+//     // tags: [ String ], 
+//     // enforce rule that tags have atleast one value:
+//     tags: {
+//         type: Array,
+//         validate: {
+//             validator: function(value) {
+//                 return value && value.length > 0 // if value exists and value.length is greater than 0, than this property is valid
+//             },
+//             message: 'A musician should have atleast one tag.' // this message will display in instance where validation criteria not met
+//         }
+//     },
+//     date: { type: Date, default: Date.now}, 
+//     isActive: Boolean,
+//     bookingPrice: {
+//         type: Number,
+//         required: function() { return this.isActive; },
+//         max: 50000
+//     }
+// });
+
+// const Musician = mongoose.model('Musician', musicianSchema);
+
+// async function createMusician() {
+//     const musician = new Musician({
+//         name: "Phoebe Bridgers",
+//         genre: "folk-rock",
+// //         tags: ['Saddest Factory', 'Punisher'],
+//         tags: [],
+//         isActive: false,
+//         bookingPrice: 10000
+//     });
+    
+//     try {
+//         const result = await musician.save();
+//         console.log("Result", result);
+//     }
+//     catch (error) {
+//         console.log("Error Message", error.message)
+//     }
+// };
+
+// createMusician();
+
+
+// // ---------------------ASYNC VALIDATORs:-----------------------------------------------
+
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost/mongo-validates-musician', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }) 
+//     .then(() => console.log('Connected to MongoDB...'))
+//     .catch(err => console.error('Could not connect to MongoDB...', err))
+
+// const musicianSchema = new mongoose.Schema({
+//     name: { 
+//         type: String, 
+//         required: true,
+//         minlength: 5,
+//         maxlength: 255,
+//     },
+//     genre: {
+//         type: String,
+//         required: true,
+//         enum: ['rock', 'grunge', 'folk-rock', 'alternative-rock', 'punk'], 
+//     }, 
+//     tags: {
+//         type: Array,
+//         validate: {
+//             isAsync: true,
+//             validator: function(value, callback) {
+//                 setTimeout(() => {
+//                     const result = value && value.length > 0;
+//                     callback(result);
+//                 }, 4000);
+//             },
+//             message: 'A musician should have at least one tag.'
+//         }
+//     },
+//     date: { type: Date, default: Date.now}, 
+//     isActive: Boolean,
+//     bookingPrice: {
+//         type: Number,
+//         required: function() { return this.isActive; },
+//         max: 50000
+//     }
+// });
+
+// const Musician = mongoose.model('Musician', musicianSchema);
+
+// async function createMusician() {
+//     const musician = new Musician({
+//         name: "Phoebe Bridgers",
+//         genre: "folk-rock",
+//         tags: ['Saddest Factory', 'Punisher'],
+//         isActive: true,
+//         bookingPrice: 10000
+//     });
+    
+//     try {
+//         const result = await musician.save();
+//         console.log("Result", result);
+//     }
+//     catch (exception) {
+//         console.log("Error Message", exception.message)
+//     }
+// };
+
+// createMusician();
+
+// // ---------------------VALIDATION ERRORS: -----------------------------------------------
+
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost/mongo-validates-musician', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }) 
+//     .then(() => console.log('Connected to MongoDB...'))
+//     .catch(err => console.error('Could not connect to MongoDB...', err))
+
+// const musicianSchema = new mongoose.Schema({
+//     name: { 
+//         type: String, 
+//         required: true,
+//         minlength: 5,
+//         maxlength: 255,
+//     },
+//     genre: {
+//         type: String,
+//         required: true,
+//         enum: ['rock', 'grunge', 'folk-rock', 'alternative-rock', 'punk'], 
+//     }, 
+//     tags: {
+//         type: Array,
+//         validate: {
+//             validator: function(value) {
+//                 return value && value.length > 0 
+//             },
+//             message: 'A musician should have atleast one tag.'
+//         }
+//     },
+//     date: { type: Date, default: Date.now}, 
+//     isActive: Boolean,
+//     bookingPrice: {
+//         type: Number,
+//         required: function() { return this.isActive; },
+//         max: 50000
+//     }
+// });
+
+// const Musician = mongoose.model('Musician', musicianSchema);
+
+// async function createMusician() {
+//     const musician = new Musician({
+//         name: "Phoebe Bridgers",
+//         genre: "-",
+//         tags: null,
+//         isActive: true,
+//         bookingPrice: 10000
+//     });
+    
+//     try {
+//         const result = await musician.save();
+//         console.log("Result", result);
+//     }
+//     catch (exception) {
+//         for (field in exception.errors) {
+//             console.log(exception.errors[field]) // or: console.log(exception.errors[field].message -> for just the error message
+//         }
+//     }
+// };
+
+// createMusician();
+
+
+// ---------------------SCHEMAtype object validations: : -----------------------------------------------
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mongo-validates-musician', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }) 
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err))
+
+const musicianSchema = new mongoose.Schema({
+    name: { 
+        type: String, 
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+    },
+    genre: {
+        type: String,
+        required: true,
+        enum: ['rock', 'grunge', 'folk-rock', 'alternative-rock', 'punk'], 
+        lowercase: true, // convert value automatically to lowercase
+        // uppercase: true, // convert value automatically to uppercase
+        trim: true, // auto remove any paddings around string
+    }, 
+    tags: {
+        type: Array,
+        validate: {
+            validator: function(value) {
+                return value && value.length > 0 
+            },
+            message: 'A musician should have atleast one tag.'
+        }
+    },
+    date: { type: Date, default: Date.now}, 
+    isActive: Boolean,
+    bookingPrice: {
+        type: Number,
+        required: function() { return this.isActive; },
+        min: 1,
+        max: 50000,
+        // custom getter and setter:
+        get: v => Math.round(v), // v <=> value // this get is especially helpful for values that might have been set before the specification was set // Setters let you transform user data before it gets to MongoDB
+        set: v => Math.round(v) // let you transform user data before it gets to MongoDB
+    }
+});
+
+const Musician = mongoose.model('Musician', musicianSchema);
+
+async function createMusician() {
+    const musician = new Musician({
+        name: "Phoebe Bridgers",
+        genre: "Folk-rock",
+        tags: ['Saddest Factory', 'Punisher'],
+        isActive: true,
+        bookingPrice: 13.3
+    });
+    
+    try {
+        const result = await musician.save();
+        console.log("Result", result);
+    }
+    catch (exception) {
+        for (field in exception.errors) {
+            console.log(exception.errors[field]) // or: console.log(exception.errors[field].message -> for just the error message
+        }
+    }
+};
+
+createMusician();
