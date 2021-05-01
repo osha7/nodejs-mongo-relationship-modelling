97032002556d9b1e964475fed4c18c38e916ac8d
osha7 <ogroetz@gmail.com>
2021-05-01 14:30:08 -0600

    first commit

---
+++ relationships.js
@@ -0,0 +1,32 @@
+
+// Mormalization: Using References
+
+let musician = {
+    name: "Phoebe Bridgers",
+}
+
+let song = {
+    musician: "608db454b7817cc1be56c60c" // id 
+}
+
+// Denormalization: Using embedded documents
+
+let song = {
+    musician: {
+        name: "Phoebe Bridgers",
+    }
+}
+
+// Hybrid
+
+let musician = {
+    name: "Phoebe Bridgers",
+    // a number of other properties
+}
+
+let song = {
+    author: {
+        id: "608db454b7817cc1be56c60c",
+        name: "Phoebe Bridgers"
+    }
+}
